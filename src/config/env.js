b78f04f82543e7feeb0723770baa4f5977e758c6
unknown <twagirimanaephron1@gmail.com>
2026-09-17 15:50:05 +0200

    db

--- src/config/env.js
+++ src/config/env.js
@@ -3,10 +3,10 @@ require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') });
 
 const env = {
   PORT: parseInt(process.env.PORT, 10) || 4000,
-  DB_HOST: process.env.DB_HOST || '127.0.0.1',
-  DB_PORT: parseInt(process.env.DB_PORT, 10) || 3306,
-  DB_USER: process.env.DB_USER || 'root',
-  DB_PASSWORD: process.env.DB_PASSWORD || 'santech@2019',
+  DB_HOST: process.env.DB_HOST || 'mysql-2edcae98-twagirimanaephron1-1032.a.aivencloud.com',
+  DB_PORT: parseInt(process.env.DB_PORT, 10) || 19822,
+  DB_USER: process.env.DB_USER || 'avnadmin',
+  DB_PASSWORD: process.env.DB_PASSWORD || 'AVNS_5Z-UQLzpGVxay-0qmeh',
   DB_NAME: process.env.DB_NAME || 'voltage_drop',
   JWT_SECRET: process.env.JWT_SECRET || 'change-me-secret',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '24h',
